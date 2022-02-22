41fa6f20fc8552cefda40bcf1f143a5bd5736057
bellaking0 <77692024+bellaking0@users.noreply.github.com>
2022-02-22 17:46:57 -0600

    commit to 'main'

---
+++ app.js
@@ -0,0 +1,12 @@
+const cat = {
+    firstName: "Craigslist",
+    lastName: "Losey", 
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }; 
+  let message = "Craigslist Losey is the best cat in the world!";
+  let result = text.toUpperCase(); 
+const names = ["Craigslist", "Sammi", "Dante", "Arlo"];
+names[names.length] = Blueberry;
+const catNames = cat.concat(names);
